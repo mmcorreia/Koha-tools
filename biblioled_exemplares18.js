b8e79b9ce445ee01911c62e9d27daf32c09cf357
mmcorreia <mmcorreia@users.noreply.github.com>
2026-08-31 00:55:50 +0100

    Update and rename biblioled_exemplares17.js to biblioled_exemplares18.js

--- biblioled_exemplares18.js
+++ biblioled_exemplares18.js
@@ -2072,7 +2072,7 @@
     intro.className = "rbmo-biblioled-intro";
     intro.appendChild(
       document.createTextNode(
-        "Aceda a este título em formato ebook com o seu cartão de leitor."
+        "Aceda a este título em formato ebook."
       )
     );
 
